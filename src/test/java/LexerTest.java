4ddafcae8b14f14bf206905db3ef0ea14b280e7c
Mateusz <mateuszdybala2@gmail.com>
2023-01-07 13:35:36 +0100

    add simple lexer test

---
+++ src/test/java/LexerTest.java
@@ -0,0 +1,39 @@
+package test.java;
+
+import main.java.parser.*;
+import main.java.exceptions.InvalidCharacterSequenceException;
+
+import java.util.ArrayList;
+
+public class LexerTest {
+    public static void main(String[] args) {
+
+
+        String input1 = "CREATE TABLE students (int id, varchar name, int age);";
+        System.out.println("------------------------------------------------------------");
+        System.out.println("Testing lexer with VALID input");
+        System.out.println("Input: " + input1);
+        testLex(input1);
+
+        String input2 = "CREATE TABLE students (int id, varchar name&surname, int age);";
+        System.out.println("------------------------------------------------------------");
+        System.out.println("Testing lexer with INVALID input");
+        System.out.println("Input: " + input2);
+        testLex(input2);
+    }
+
+    public static void testLex(String input) {
+        ArrayList<Token> tokens = new ArrayList<>();
+        Lexer l = new Lexer(input);
+
+        try {
+            tokens = l.lex();
+        } catch (InvalidCharacterSequenceException e) {
+            e.printStackTrace();
+        }
+
+        for (Token t : tokens) {
+            System.out.println(t);
+        }
+    }
+}
